7de7da1fa06c3e36cdc0557ad994eae6f2443a83
Joey Mar Antonio <jom16antonio@gmail.com>
2017-01-24 10:49:12 +0800

    SDK-30 Update Java SDK to support latest Verification endpoint.

--- LenddoApi/src/com/lenddo/javaapi/models/ClientVerification.java
+++ LenddoApi/src/com/lenddo/javaapi/models/ClientVerification.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 /**
  * Created by Joey Mar Antonio on 12/8/15.
+ * Updated: 01/24/2017 <j.antonio> SDK-30
  */
 public class ClientVerification {
     public String partner_script_id;
@@ -16,14 +17,18 @@ public class ClientVerification {
     public String _id;
     public String partner_id;
     public Probes probes;
-
+// start: SDK-30 <j.antonio> Added verified_by_facebook boolean flag
+    public Boolean verified_by_facebook;
+// end: SDK-30 <j.antonio>
 
     public static class Verifications {
 
         public Boolean name;
         public Boolean university;
         public Boolean employer;
-        public Boolean facebook_verified;
+// start: SDK-30 <j.antonio> Remove verifications.facebook_verified
+//        public Boolean facebook_verified;
+// end: SDK-30 <j.antonio>
         public Boolean birthday;
         public Object top_employer;
 
@@ -34,7 +39,9 @@ public class ClientVerification {
         public List<String> name = new ArrayList<String>();
         public University university;
         public Employer employer;
-        public List<String> facebook_verified = new ArrayList<String>();
+// SDK-30 <j.antonio> Remove probes.facebook_verified
+//        public List<String> facebook_verified = new ArrayList<String>();
+// end: SDK-30 <j.antonio>
         public List<Integer> birthday = new ArrayList<Integer>();
         public Object top_employer;
 
