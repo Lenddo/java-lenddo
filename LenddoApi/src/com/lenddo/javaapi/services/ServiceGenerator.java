b256c5561311eb0386403903e05396562c371541
Joey Mar Antonio <jom16antonio@gmail.com>
2017-03-10 06:25:23 +0800

    remove commented out codes

--- LenddoApi/src/com/lenddo/javaapi/services/ServiceGenerator.java
+++ LenddoApi/src/com/lenddo/javaapi/services/ServiceGenerator.java
@@ -4,21 +4,9 @@ import okhttp3.CipherSuite;
 import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
 import okhttp3.TlsVersion;
-import okio.Buffer;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.Retrofit;
 
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.GeneralSecurityException;
-import java.security.KeyStore;
-import java.security.SecureRandom;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
-import java.util.Collection;
 import java.util.Collections;
 
 /**
@@ -47,10 +35,8 @@ public class ServiceGenerator {
     private static Retrofit.Builder builder = new Retrofit.Builder()
                                                     .addConverterFactory(GsonConverterFactory.create());
     public static <S> S createService(Class<S> serviceClass, String baseUrl) {
-//        SSLContext sslContext = sslContextForTrustedCertificates(trustedCertificatesInputStream());
         httpClient = new OkHttpClient.Builder()
                 .connectionSpecs(Collections.singletonList(spec))
-//                .sslSocketFactory(sslContext.getSocketFactory())
                 .build();
         retrofit = builder.client(httpClient)
                             .baseUrl(baseUrl)
@@ -61,108 +47,4 @@ public class ServiceGenerator {
     public static Retrofit getRetrofit() {
         return retrofit;
     }
-//
-//    protected static SSLContext sslContextForTrustedCertificates(InputStream in) {
-//        try {
-//            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-//            Collection<? extends Certificate> certificates = certificateFactory.generateCertificates(in);
-//            if (certificates.isEmpty()) {
-//                throw new IllegalArgumentException("expected non-empty set of trusted certificates");
-//            }
-//
-//            // Put the certificates a key store.
-//            char[] password = "l3ndd0".toCharArray(); // Any password will work.
-//            KeyStore keyStore = newEmptyKeyStore(password);
-//            int index = 0;
-//            for (Certificate certificate : certificates) {
-//                String certificateAlias = Integer.toString(index++);
-//                keyStore.setCertificateEntry(certificateAlias, certificate);
-//            }
-//
-//            // Wrap it up in an SSL context.
-//            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(
-//                    KeyManagerFactory.getDefaultAlgorithm());
-//            keyManagerFactory.init(keyStore, password);
-//            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
-//                    TrustManagerFactory.getDefaultAlgorithm());
-//            trustManagerFactory.init(keyStore);
-//            SSLContext sslContext = SSLContext.getInstance("TLS");
-//            sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(),
-//                    new SecureRandom());
-//            return sslContext;
-//        } catch (GeneralSecurityException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    private static KeyStore newEmptyKeyStore(char[] password) throws GeneralSecurityException {
-//        try {
-//            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-//            InputStream in = null; // By convention, 'null' creates an empty key store.
-//            keyStore.load(in, password);
-//            return keyStore;
-//        } catch (IOException e) {
-//            throw new AssertionError(e);
-//        }
-//    }
-
-//    private static InputStream trustedCertificatesInputStream() {
-//        String gdig2 = ""
-//                + "-----BEGIN CERTIFICATE-----\n"
-//                + "MIIE0DCCA7igAwIBAgIBBzANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UEBhMCVVMx\n"
-//                + "EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT\n"
-//                + "EUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290IENlcnRp\n"
-//                + "ZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTExMDUwMzA3MDAwMFoXDTMxMDUwMzA3\n"
-//                + "MDAwMFowgbQxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQH\n"
-//                + "EwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjEtMCsGA1UE\n"
-//                + "CxMkaHR0cDovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMTMwMQYDVQQD\n"
-//                + "EypHbyBEYWRkeSBTZWN1cmUgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IC0gRzIwggEi\n"
-//                + "MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC54MsQ1K92vdSTYuswZLiBCGzD\n"
-//                + "BNliF44v/z5lz4/OYuY8UhzaFkVLVat4a2ODYpDOD2lsmcgaFItMzEUz6ojcnqOv\n"
-//                + "K/6AYZ15V8TPLvQ/MDxdR/yaFrzDN5ZBUY4RS1T4KL7QjL7wMDge87Am+GZHY23e\n"
-//                + "cSZHjzhHU9FGHbTj3ADqRay9vHHZqm8A29vNMDp5T19MR/gd71vCxJ1gO7GyQ5HY\n"
-//                + "pDNO6rPWJ0+tJYqlxvTV0KaudAVkV4i1RFXULSo6Pvi4vekyCgKUZMQWOlDxSq7n\n"
-//                + "eTOvDCAHf+jfBDnCaQJsY1L6d8EbyHSHyLmTGFBUNUtpTrw700kuH9zB0lL7AgMB\n"
-//                + "AAGjggEaMIIBFjAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBBjAdBgNV\n"
-//                + "HQ4EFgQUQMK9J47MNIMwojPX+2yz8LQsgM4wHwYDVR0jBBgwFoAUOpqFBxBnKLbv\n"
-//                + "9r0FQW4gwZTaD94wNAYIKwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8v\n"
-//                + "b2NzcC5nb2RhZGR5LmNvbS8wNQYDVR0fBC4wLDAqoCigJoYkaHR0cDovL2NybC5n\n"
-//                + "b2RhZGR5LmNvbS9nZHJvb3QtZzIuY3JsMEYGA1UdIAQ/MD0wOwYEVR0gADAzMDEG\n"
-//                + "CCsGAQUFBwIBFiVodHRwczovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkv\n"
-//                + "MA0GCSqGSIb3DQEBCwUAA4IBAQAIfmyTEMg4uJapkEv/oV9PBO9sPpyIBslQj6Zz\n"
-//                + "91cxG7685C/b+LrTW+C05+Z5Yg4MotdqY3MxtfWoSKQ7CC2iXZDXtHwlTxFWMMS2\n"
-//                + "RJ17LJ3lXubvDGGqv+QqG+6EnriDfcFDzkSnE3ANkR/0yBOtg2DZ2HKocyQetawi\n"
-//                + "DsoXiWJYRBuriSUBAA/NxBti21G00w9RKpv0vHP8ds42pM3Z2Czqrpv1KrKQ0U11\n"
-//                + "GIo/ikGQI31bS/6kA1ibRrLDYGCD+H1QQc7CoZDDu+8CL9IVVO5EFdkKrqeKM+2x\n"
-//                + "LXY2JtwE65/3YR8V3Idv7kaWKK2hJn0KCacuBKONvPi8BDAB\n"
-//                + "-----END CERTIFICATE-----";
-//        String gdrootg2 = ""
-//                + "-----BEGIN CERTIFICATE-----\n"
-//                + "MIIDxTCCAq2gAwIBAgIBADANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UEBhMCVVMx\n"
-//                + "EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT\n"
-//                + "EUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290IENlcnRp\n"
-//                + "ZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTA5MDkwMTAwMDAwMFoXDTM3MTIzMTIz\n"
-//                + "NTk1OVowgYMxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQH\n"
-//                + "EwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjExMC8GA1UE\n"
-//                + "AxMoR28gRGFkZHkgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkgLSBHMjCCASIw\n"
-//                + "DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAL9xYgjx+lk09xvJGKP3gElY6SKD\n"
-//                + "E6bFIEMBO4Tx5oVJnyfq9oQbTqC023CYxzIBsQU+B07u9PpPL1kwIuerGVZr4oAH\n"
-//                + "/PMWdYA5UXvl+TW2dE6pjYIT5LY/qQOD+qK+ihVqf94Lw7YZFAXK6sOoBJQ7Rnwy\n"
-//                + "DfMAZiLIjWltNowRGLfTshxgtDj6AozO091GB94KPutdfMh8+7ArU6SSYmlRJQVh\n"
-//                + "GkSBjCypQ5Yj36w6gZoOKcUcqeldHraenjAKOc7xiID7S13MMuyFYkMlNAJWJwGR\n"
-//                + "tDtwKj9useiciAF9n9T521NtYJ2/LOdYq7hfRvzOxBsDPAnrSTFcaUaz4EcCAwEA\n"
-//                + "AaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYwHQYDVR0OBBYE\n"
-//                + "FDqahQcQZyi27/a9BUFuIMGU2g/eMA0GCSqGSIb3DQEBCwUAA4IBAQCZ21151fmX\n"
-//                + "WWcDYfF+OwYxdS2hII5PZYe096acvNjpL9DbWu7PdIxztDhC2gV7+AJ1uP2lsdeu\n"
-//                + "9tfeE8tTEH6KRtGX+rcuKxGrkLAngPnon1rpN5+r5N9ss4UXnT3ZJE95kTXWXwTr\n"
-//                + "gIOrmgIttRD02JDHBHNA7XIloKmf7J6raBKZV8aPEjoJpL1E/QYVN8Gb5DKj7Tjo\n"
-//                + "2GTzLH4U/ALqn83/B2gX2yKQOC16jdFU8WnjXzPKej17CuPKf1855eJ1usV2GDPO\n"
-//                + "LPAvTK33sefOT6jEm0pUBsV/fdUID+Ic/n4XuKxe9tQWskMJDE32p2u0mYRlynqI\n"
-//                + "4uJEvlz36hz1\n"
-//                + "-----END CERTIFICATE-----\n";
-//        return new Buffer()
-//                .writeUtf8(gdig2)
-//                .writeUtf8(gdrootg2)
-//                .inputStream();
-//    }
 }
